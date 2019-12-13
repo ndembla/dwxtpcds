aabc690ccc19ff1038ace317bb81d7a1696d91e5
Mustafa Iman <mustafaiman@gmail.com>
2019-12-13 11:53:25 -0800

    DynamoDB scans only first 1MB of items in a table. When table is larger than that, manual pagination is required

--- updatedyndb.py
+++ updatedyndb.py
@@ -1,3 +1,4 @@
+#!/bin/python
 import boto3
 from botocore.exceptions import ClientError
 
@@ -5,38 +6,45 @@ dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
 table = dynamodb.Table('dwxtpcds-9n4f-dwx-managed')
 
-response = table.scan(
-ProjectionExpression='#k,#s',
-ExpressionAttributeNames={
-'#k' : 'parent', #partition key
-'#s' : 'child' #sort key
-}
-)
+exclusiveStartKey = None
 
-items = response['Items']
-
-for item in items:
-try:
- response = table.update_item(
- Key=item,
- UpdateExpression='SET #a = :ath',
- ConditionExpression='#d = :t and NOT begins_with(#s, :tmp) and #a = :f',
- ExpressionAttributeNames={
- "#d" : 'is_dir',
- "#a" : 'is_authoritative',
- "#s" : 'child'
- },
- ExpressionAttributeValues={
- ":ath" : True,
- ":t" : True,
- ":f" : False,
- ":tmp": '_tmp'
- }
- )
- print("Updated item")
-except ClientError as e:
-    if e.response['Error']['Code'] == "ConditionalCheckFailedException":
-print ("Item ignored ")
-continue
+while True:
+    print("Start key is " + str(exclusiveStartKey))
+    if exclusiveStartKey == None:
+        response = table.scan(
+            FilterExpression='is_dir=:t AND is_authoritative=:f AND NOT begins_with(child, :cbw)',
+            ExpressionAttributeValues={ ":t": True, ":f": False, ":cbw": "_tmp"}
+        )
     else:
-raise
+        response = table.scan(
+            FilterExpression='is_dir=:t AND is_authoritative=:f AND NOT begins_with(child, :cbw)',
+            ExpressionAttributeValues={ ":t": True, ":f": False, ":cbw": "_tmp"},
+            ExclusiveStartKey=exclusiveStartKey
+        )
+    items=response['Items']
+    if 'LastEvaluatedKey' in response:
+        exclusiveStartKey=response['LastEvaluatedKey']
+    else:
+        exclusiveStartKey=None
+    print("Found " + str(len(items)) + " items to update")
+    print("Last key evaluated in this batch is " + str(exclusiveStartKey))
+    for item in items:
+        try:
+            response = table.update_item(
+                Key={"parent": item['parent'], "child": item['child']},
+                UpdateExpression='SET #is_aut = :is_aut',
+                ExpressionAttributeNames={
+                    "#is_aut": 'is_authoritative'
+                },
+                ExpressionAttributeValues={
+                    ":is_aut": True
+                }
+            )
+        except ClientError as e:
+            if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+                print("Item ignored ")
+                continue
+            else:
+                raise
+    if exclusiveStartKey == None:
+        break
