e16b3106a5768f0dbb0c189d2672dc5b5dc28b72
Mustafa Iman <mustafaiman@gmail.com>
2020-02-07 11:18:11 -0800

    Ignore /tmp /_tmp and /das/ patterns in updating dynamodb tables

--- updatedyndb.py
+++ updatedyndb.py
@@ -2,9 +2,11 @@
 import boto3
 from botocore.exceptions import ClientError
 
+DEBUG=True
+
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
-table = dynamodb.Table('dwxtpcds-9n4f-dwx-managed')
+table = dynamodb.Table('dwxtpcds30-wwgq-dwx-managed')
 
 exclusiveStartKey = None
 
@@ -12,13 +14,13 @@ while True:
     print("Start key is " + str(exclusiveStartKey))
     if exclusiveStartKey == None:
         response = table.scan(
-            FilterExpression='is_dir=:t AND is_authoritative=:f AND NOT begins_with(child, :cbw)',
-            ExpressionAttributeValues={ ":t": True, ":f": False, ":cbw": "_tmp"}
+            FilterExpression='is_dir=:t AND is_authoritative=:f AND NOT begins_with(child, :cbw) AND NOT begins_with(child, :cbw2)',
+            ExpressionAttributeValues={ ":t": True, ":f": False, ":cbw": "_tmp", ":cbw2": "tmp"}
         )
     else:
         response = table.scan(
-            FilterExpression='is_dir=:t AND is_authoritative=:f AND NOT begins_with(child, :cbw)',
-            ExpressionAttributeValues={ ":t": True, ":f": False, ":cbw": "_tmp"},
+            FilterExpression='is_dir=:t AND is_authoritative=:f AND NOT begins_with(child, :cbw) AND NOT begins_with(child, :cbw2)',
+            ExpressionAttributeValues={ ":t": True, ":f": False, ":cbw": "_tmp", ":cbw2": "tmp"},
             ExclusiveStartKey=exclusiveStartKey
         )
     items=response['Items']
@@ -26,9 +28,13 @@ while True:
         exclusiveStartKey=response['LastEvaluatedKey']
     else:
         exclusiveStartKey=None
-    print("Found " + str(len(items)) + " items to update")
     print("Last key evaluated in this batch is " + str(exclusiveStartKey))
+    itemsUpdated = 0
     for item in items:
+        if item['parent'].find('/tmp') > -1 or item['parent'].find('/_tmp') > -1 and item['parent'].find('/das/') > -1:
+            continue
+        if DEBUG:
+            print item['parent'], item['child']
         try:
             response = table.update_item(
                 Key={"parent": item['parent'], "child": item['child']},
@@ -40,11 +46,14 @@ while True:
                     ":is_aut": True
                 }
             )
+            itemsUpdated = itemsUpdated + 1
         except ClientError as e:
             if e.response['Error']['Code'] == "ConditionalCheckFailedException":
                 print("Item ignored ")
                 continue
             else:
                 raise
+        
+    print("Updated " + str(itemsUpdated) + " items")
     if exclusiveStartKey == None:
         break
